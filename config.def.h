67959015cce1527770b7a756f49e0eb6f410dddf
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-08-08 14:30:30 -0500

    Install high priority patch

--- config.def.h
+++ config.def.h
@@ -16,6 +16,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeHp]  = { "#bbbbbb", "#333333" }
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
